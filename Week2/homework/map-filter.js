3ab643df870bcabe2e55304cd2c7068cc5a8d4bc
diana-dodan <148563980+diana-dodan@users.noreply.github.com>
2024-01-19 00:19:40 +0100

    Update map-filter.js

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,12 +1,22 @@
 'use strict';
 
-function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
-}
+function throwAway(numbers) {
+  return numbers.filter(number => {
+    if (number % 2 === 0) {
+      return false;
+    }
 
+    return number;
+  });
+}
 const myNumbers = [1, 2, 3, 4];
-console.log(doubleOddNumbers(myNumbers));
+console.log(throwAway(myNumbers));
+
+function doubleOddNumbers(numbers) {
+  return numbers.map(number => number * 2);
+}
+const newNumbers = throwAway(myNumbers);
+console.log(doubleOddNumbers(newNumbers));
 
 // Do not change or remove anything below this line
 module.exports = {
